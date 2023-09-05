aa8044fe4695015a494980e9d228cde28c95e42e
yandah1 <nonku003@gmail.com>
2023-09-06 01:21:47 +0200

    Inventing is a combination of brains and materials. The more brains you use, the less material you need.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for digit1 in range(10):
     for digit2 in range(digit1 + 1, 10):
-        print(f"{digit1:02d}, {digit2:02d}", end=', ')
+        print("{:02d}, {:02d}".format(digit1, digit2), end=', ')
 print()
