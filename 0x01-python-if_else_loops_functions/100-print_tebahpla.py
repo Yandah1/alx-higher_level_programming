6567a0436afa5c8bc062ab7c1ca38cfa147eb1cb
yandah1 <nonku003@gmail.com>
2023-09-06 22:39:02 +0200

    done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-def print_reverse_alphabet():
-    for c in range(ord('Z'), ord('A') - 1, -1):
-        print("{}".format(chr(c)), end="")
-        if c != ord('A'):
-            print("{}".format(chr(c + 32)), end="")
-    print()
+for c in range(122, 96, -1):
+        if c % 2 != 0:
+            c = c - 32
+            print("{}".format(chr(c)), end="")
