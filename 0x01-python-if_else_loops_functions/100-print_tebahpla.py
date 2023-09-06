85d78ef5b56f517939301591701add89ad21a511
yandah1 <nonku003@gmail.com>
2023-09-07 00:25:22 +0200

    Smile in the mirror..

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -2,6 +2,6 @@
 
 for c in range(122, 96, -1):
     if c % 2 != 0:
-        print("{}".format(chr(c)), end="")
-    else:
         print("{}".format(chr(c - 32)), end="")
+    else:
+        print("{}".format(chr(c)), end="")
