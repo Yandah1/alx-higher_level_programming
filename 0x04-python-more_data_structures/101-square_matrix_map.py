7f2242a14b4153875eddf09e79aa5f2c8ad79e5f
yandah1 <nonku003@gmail.com>
2023-09-14 00:58:04 +0200

    Squared by using map

---
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+
+    weighted_sum = sum(x * y for x, y in my_list)
+    total_weight = sum(y for _, y in my_list)
+
+    if total_weight == 0:
+        return 0
+
+    return weighted_sum / total_weight
