5cb9f26785c62eb2d5c626e3267663a005be2696
yandah1 <nonku003@gmail.com>
2023-10-03 00:22:55 +0200

    redone.

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle:
         """Initialize a  Rectangle.
 
         Args:
-            width(int): width of a  rectangle
+            width(int): width of a rectangle
             hight(int): hieght of a rectangle
         """
         self.width = width
@@ -53,7 +53,7 @@ class Rectangle:
         Raises:
             TypeError: If the value is not an integer.
             ValueError: If the value is less than 0.
-        """ 
+        """
 
         if type(value) is not int:
             raise TypeError("height must be an integer")
