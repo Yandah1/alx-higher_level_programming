93e5851864b50d3658b5ce2e7a2de662c2c92266
yandah1 <nonku003@gmail.com>
2023-10-16 20:58:14 +0200

    First Rectangle

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -13,8 +13,8 @@ class Base:
             id (int): The ID of the object.
         """
 
-        if id  is not None:
-            self.id =  id
+        if id is not None:
+            self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
