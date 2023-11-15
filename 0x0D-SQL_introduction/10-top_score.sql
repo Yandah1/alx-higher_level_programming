4c937230c95d068c548dc64c708378dcace5a170
yandah1 <nonku003@gmail.com>
2023-11-15 04:27:22 +0200

    Fixed again

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,4 +1,5 @@
---lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
+-- Records should be ordered by score (top first)
 SELECT score, name
 FROM second_table
 ORDER BY score DESC;
