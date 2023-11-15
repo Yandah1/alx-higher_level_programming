614b6dc30b196cb623d8003598334cff8ddeba89
yandah1 <nonku003@gmail.com>
2023-11-16 00:53:45 +0200

    tv.title

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,5 @@
 -- lists all shows contained in the database hbtn_0d_tvshows
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows tvs
-LEFT JOIN tv_show_genres tv_show_genres
-ON tv_genres.show_id = tv_show.id OR tv_genres.show_id = NULL
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows AS s
+LEFT JOIN tv_show_genres AS g
+ON tv_genres.show_id = tv_show.id
 ORDER BY tv_show.title, tv_show_genres.genre_id ASC;
