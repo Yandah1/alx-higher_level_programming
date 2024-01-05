3bb7903cf71ff5832bdc5a720ac82586a79c3bb6
yandah1 <nonku003@gmail.com>
2024-01-05 22:15:19 +0200

    curl body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# Ascript that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-curl -sI "$1" | wc -c
+# Ascript that takes in a URL, sends a request to that URL, a, and displays the size of the body of the respon
+curl -sI "$1" | grep -i Content-Length | cut -d ' ' -f2
