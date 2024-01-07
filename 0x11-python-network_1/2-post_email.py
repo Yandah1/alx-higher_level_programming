3fc6f1ff53c4a04336dceeabb9438a6c1251a5f5
yandah1 <nonku003@gmail.com>
2024-01-07 23:18:38 +0200

    POST an email #0

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
-'''This script sends a POST request to the provided URL with an email as a parameter and displays the response body.''''
+'''
+This script sends a POST request to the provided URL with an email
+as a parameter and displays the response body.
+'''
+
+import sys
+import urllib.parse
+import urllib.request
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    with urllib.request.urlopen(url, data) as response:
+        body = response.read().decode('utf-8')
+        print(body)
