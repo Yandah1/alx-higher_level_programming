d81b8f6158e063f76335fc68a73dc77267c1ff4b
yandah1 <nonku003@gmail.com>
2024-01-08 00:22:34 +0200

    . Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
-''' This script sends a request to the provided URL and displays the response body. '''
+'''
+This script sends a request to the provided URL and displays the response body.
+'''
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    response = requests.get(url)
+    if response.status_code >= 400:
+        print('Error code:', response.status_code)
+    else:
+        print(response.text)
