c05f782d1acf57031dc2159388ba998e61b5db26
yandah1 <nonku003@gmail.com>
2024-01-08 01:09:29 +0200

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
-'''This script uses the GitHub API to display the user's ID by authenticating with a personal access token.'''
+'''
+This script uses the GitHub API to display the user's ID by
+authenticating with a personal access token.
+'''
+
+import sys
+import requests
+
+if __name__ == "__main__":
+
+    username = sys.argv[1]
+    access_token = sys.argv[2]
+
+    url = f"https://api.github.com/user"
+    headers = {
+            'Authorization': 'access_token {}'.format(access_token)
+            }
+    response = requests.get(url, headers=headers)
+    print(response.json().get('id', 'None'))
