060bb4b583c57ed96ec15ad7a544f90917d48680
yandah1 <nonku003@gmail.com>
2024-01-07 23:46:20 +0200

    What's my status? #1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-''' This script fetches the status from the provided URL usingthe requests package.'''
+'''
+This script fetches the status from the provided URL usingthe requests package.
+'''
+
+import requests
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    res = requests.get(url)
+    print('Body response:')
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- utf8 content: {}".format(res.text))
