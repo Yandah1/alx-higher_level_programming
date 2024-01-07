fcde73d36064c0fc7519cdea398c68d88a190cc0
yandah1 <nonku003@gmail.com>
2024-01-07 23:34:20 +0200

    Error code #0

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
-''' This script  takes a URL as input.'''
+'''
+This script sends a request to the specified URL and displays the body of the response
+(decoded in utf-8).
+'''
 
 import sys
-import urllib.parse
-import urllib.request
+import urllib.error
+import urllib.request as request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    try:
+        with request.urlopen(url) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as er:
+        print('Error code:', er.code)
