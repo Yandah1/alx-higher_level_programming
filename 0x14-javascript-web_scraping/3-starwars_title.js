4f9c6d271a8aabc71e83198860372e2da60ab619
yandah1 <nonku003@gmail.com>
2024-02-01 01:16:46 +0200

    Star wars movie title

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+// Write a script that prints the title of a Star Wars movie where the episode
+// number matches a given integer. The first argument is the movie ID
+
+const request = require('request');
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
